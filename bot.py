882c1544c34e3f624a8d374992588e23a6dce4cc
smsov1988-cell <smsov1988@gmail.com>
2025-09-23 03:18:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -176,4 +176,14 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(main())
+
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if "already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())
+            loop.run_forever()
+        else:
+            raise
+
